7cbc2098fe3a6657fa8393a67c54023605d36eb0
Mihir Mange <mihir@initialyze.com>
2020-11-10 16:33:24 -0800

    updated example javascript and sample pages

--- ui.frontend/src/main/webpack/site/dynamic-components.js
+++ ui.frontend/src/main/webpack/site/dynamic-components.js
@@ -1,17 +1,35 @@
+//register the className of each dynamic component to it's own js chunk, and initialization call
+const dynamicComponents = {
+    "cmp-carousel": async () => {
+        await import(/* webpackChunkName: 'cmp-carousel' */ "../components/content/carousel/js/carousel.js").then(obj => obj.default.init())},
+
+    "helloworld": async () => {
+        await import(/* webpackChunkName: 'cmp-helloworld' */ "../components/content/helloworld/ts/helloworld.js").then(obj => obj.default.init())},
+}
+
+const alreadyLoadedScripts = {};
+
+//  Dynamic Components Loader
+//
+//  Loops through dynamic components registered, if found on page, loads their chunk dynamically.
 const DynamicComponents = {
-    dynamicComponents : {
-        "cmp-carousel": import(/* webpackChunkName: 'carousel' */ "../components/content/carousel/js/carousel.js"),
-        "helloworld": import(/* webpackChunkName: 'hello-world' */"../components/content/helloworld/ts/helloworld.js")
-    },
     init: function () {
-        const { dynamicComponents } = this;
-        Object.keys(dynamicComponents).forEach(function(dynamicComponent){
-            const componentContext = document.querySelector(`.${dynamicComponent}`);
+        //loop through each registered dynamic component
+        Object.keys(dynamicComponents).forEach(function(component){
+            const componentContext = document.querySelector(`.${component}`);
+
+            //if component found on page
             if(componentContext) {
-                dynamicComponents[dynamicComponent]
-                .then(function(component){
-                    component.default.init();
-                });
+
+                //check if script not already loaded
+                if (!alreadyLoadedScripts[component]) {
+
+                    //record script state
+                    alreadyLoadedScripts[component] = true;
+
+                    //execute the dynamic import & init script registered
+                    dynamicComponents[component]();
+                }
             }
         });
     } 
